75140408e80e8936a50539d51a9188c1f796e905
pangyoanoangyo <pil1983@naver.com>
2022-02-10 19:14:22 +0900

    first commit

---
+++ page_1.py
@@ -0,0 +1,18 @@
+def sangfeel():
+    print("그는 열심히 노력중입니다")
+
+def sadyou():
+    for i in range(1,10):
+        print("당신은 {0}번 차였습니다".format(i))
+
+def wtf(a,b,c):
+    for a in range(2,6):
+        for i in range(0,4):
+            print("당신의 {0}번째 로또 당첨금은 {1} 억 입니다.".format(a,i))
+    print("당신의 키는",b,"cm입니다")            
+    c = a + b
+    print(c)
+
+    
+
+
---
+++ page_2.py
@@ -0,0 +1,10 @@
+import page_1
+
+a = page_1.sangfeel()
+b = page_1.sadyou(1)
+print(a)
+print(b)
+
+
+
+
